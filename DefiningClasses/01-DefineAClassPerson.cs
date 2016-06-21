284f1748c10de2054fc41b494efb5fd183a3473a
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-21 10:05:19 +0300

    Add files via upload

---
+++ DefiningClasses/01-DefineAClassPerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+class Person
+{
+    public string name;
+    public int age;
+}
+
+class DefineAClassPerson
+{
+    static void Main(string[] args)
+    {
+        Type personType = typeof(Person);
+        FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+        Console.WriteLine(fields.Length);
+    }
+}
