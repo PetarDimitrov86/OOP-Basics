37ff65a513b088f272eeaf5d4c0f77615a8cd3fc
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-21 11:29:50 +0300

    Add files via upload

---
+++ DefiningClasses/02-CreatingConstructors.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+class Person
+{
+    public string name;
+    public int age;
+
+    public Person()
+    {
+        this.name = "No name";
+        this.age = 1;
+    }
+
+    public Person(int age)
+    {
+        this.name = "No name";
+        this.age = age;
+    }
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class CreatingConstructors
+{
+    static void Main(string[] args)
+    {
+        Type personType = typeof(Person);
+        ConstructorInfo emptyCtor = personType.GetConstructor(new Type[] { });
+        ConstructorInfo ageCtor = personType.GetConstructor(new[] { typeof(int) });
+        ConstructorInfo nameAgeCtor = personType.GetConstructor(new[] { typeof(string), typeof(int) });
+        bool swapped = false;
+        if (nameAgeCtor == null)
+        {
+            nameAgeCtor = personType.GetConstructor(new[] { typeof(int), typeof(string) });
+            swapped = true;
+        }
+
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+
+        Person basePerson = (Person)emptyCtor.Invoke(new object[] { });
+        Person personWithAge = (Person)ageCtor.Invoke(new object[] { age });
+        Person personWithAgeAndName = swapped ? (Person)nameAgeCtor.Invoke(new object[] { age, name }) : (Person)nameAgeCtor.Invoke(new object[] { name, age });
+
+        Console.WriteLine("{0} {1}", basePerson.name, basePerson.age);
+        Console.WriteLine("{0} {1}", personWithAge.name, personWithAge.age);
+        Console.WriteLine("{0} {1}", personWithAgeAndName.name, personWithAgeAndName.age);
+    }
+}
