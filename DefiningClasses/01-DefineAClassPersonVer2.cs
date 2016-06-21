f92c6e8a4269f474b4ceab57e68db35bdf6a1196
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-21 10:09:43 +0300

    Version2 with a constructor

---
+++ DefiningClasses/01-DefineAClassPersonVer2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+class Person
+{
+    public string name;
+    public int age;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class DefineAClassPerson
+{
+    static void Main(string[] args)
+    {
+        Type personType = typeof(Person);
+        FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+        Console.WriteLine(fields.Length);
+
+        Person firstPerson = new Person("Pesho", 20);
+
+        Console.WriteLine(firstPerson.age);
+        Console.WriteLine(firstPerson.name);
+    }
+}
