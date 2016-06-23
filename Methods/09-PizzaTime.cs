2a4a40c3968c7a650cf33153e6ee12a8a5e90767
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-23 15:30:10 +0300

    Add files via upload

---
+++ Methods/09-PizzaTime.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class Pizza
+{
+    public string name;
+    public int group;
+
+    public Pizza(string name, int group)
+    {
+        this.name = name;
+        this.group = group;
+    }
+}
+public class PizzaTime
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string[] inputSplit = input.Split();
+        Pizza[] pizzas = new Pizza[inputSplit.Length];
+        string pattern = @"(\d+)(\w+)";
+        Regex regex = new Regex(pattern);
+        for (int i = 0; i < inputSplit.Length; i++)
+        {
+            Match match = regex.Match(inputSplit[i]);
+            int pizzaGroup = int.Parse(match.Groups[1].Value);
+            string pizzaName = match.Groups[2].Value;
+            Pizza ourPizza = new Pizza(pizzaName, pizzaGroup);
+            pizzas[i] = ourPizza;
+        }
+        var result = GetDictionary(pizzas);
+        foreach (var outerPair in result)
+        {
+            Console.Write("{0} - ", outerPair.Key);
+            Console.WriteLine(string.Join(", ", outerPair.Value ));
+        }
+    }
+    public static SortedDictionary<int, List<string>> GetDictionary(params Pizza[] pizzas)
+    {
+        SortedDictionary<int, List<string>> ourDict = new SortedDictionary<int, List<string>>();
+        foreach (var pizza in pizzas)
+        {
+            if (!ourDict.ContainsKey(pizza.group))
+            {
+                ourDict.Add(pizza.group, new List<string>());
+            }
+            ourDict[pizza.group].Add(pizza.name);
+        }
+        return ourDict;
+    }
+}
