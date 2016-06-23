e08f9efeb22cd5b9d2d721579efcfe7a970143f8
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-23 09:13:00 +0300

    Add files via upload

---
+++ Methods/04-NumberInReversedOrder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+public class DecimalNumber
+{
+    public decimal number;
+
+    public DecimalNumber(decimal number)
+    {
+        this.number = number;
+    }
+
+    public decimal ReverseNumbers()
+    {
+        string decimalAsString = number.ToString();
+        StringBuilder reversed = new StringBuilder();
+        for (int i = decimalAsString.Length - 1; i >= 0 ; i--)
+        {
+            reversed.Append(decimalAsString[i]);
+        }
+        return decimal.Parse(reversed.ToString());
+    }
+}
+
+class NumberInReversedOrder
+{
+    static void Main(string[] args)
+    {
+        decimal n = decimal.Parse(Console.ReadLine());
+        DecimalNumber decN = new DecimalNumber(n);
+        Console.WriteLine(decN.ReverseNumbers());
+    }
+}
