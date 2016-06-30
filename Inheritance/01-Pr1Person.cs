3525d910de6701fa99cea887162cc5c456e1f277
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-30 13:11:03 +0300

    Add files via upload

---
+++ Inheritance/01-Pr1Person.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+
+public class Person
+{
+    public string name;
+    public int age;
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public virtual string Name
+    {
+        get { return this.name; }
+        protected set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Name's length should not be less than 3 symbols!");
+            }
+            this.name = value;
+        }
+    }
+
+    public virtual int Age
+    {
+        get { return this.age; }
+        protected set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Age must be positive!");
+            }
+            this.age = value;
+        }
+    }
+    public override string ToString()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                             this.Name,
+                             this.Age));
+
+        return stringBuilder.ToString();
+    }
+}
+
+public class Child : Person
+{
+    public Child(string name, int age)
+        :base(name, age)
+    {
+    }
+
+    public override int Age
+    {
+        get { return base.Age; }
+        protected set
+        {
+            if (value > 15)
+            {
+                throw new ArgumentException("Child's age must be less than 15!");
+            }
+            base.Age = value;
+        }
+    }
+}
+
+class Pr1Person
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+
+        try
+        {
+            Child child = new Child(name, age);
+            Console.WriteLine(child);
+        }
+        catch (ArgumentException ae)
+        {
+            Console.WriteLine(ae.Message);
+        }
+    }
+}
