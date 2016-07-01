f775d7a20f1ab9fdceb88244d4a97f26cc6c4a94
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-01 13:28:04 +0300

    Add files via upload

---
+++ Methods/12-PrintPeople.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Person : IComparable<Person>
+{
+    public string name;
+    public int age;
+    public string occupation;
+
+    public Person(string name, int age, string occupation)
+    {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.name} - age: {this.age}, occupation: {this.occupation}";
+    }
+
+    public int CompareTo(Person anotherPers)
+    {
+        return this.age.CompareTo(anotherPers.age);
+    }
+}
+
+class PrintPeople
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        List<Person> people = new List<Person>();
+        while (input != "END")
+        {
+            string[] personInfo = input.Split();
+            string name = personInfo[0];
+            int age = int.Parse(personInfo[1]);
+            string occupation = personInfo[2];
+            Person ourPerson = new Person(name, age, occupation);
+            people.Add(ourPerson);
+            input = Console.ReadLine();
+        }
+        people.Sort();
+        foreach (var person in people)
+        {
+            Console.WriteLine(person);
+        }
+    }
+}
