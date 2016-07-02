29567b2c6adfefc28a1ec266fb0d9eb70a24092f
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-02 18:08:20 +0300

    Add files via upload

---
+++ StaticMembers/01-Pr1Students.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Student
+{
+    public static int numberOfStudents = 0;
+    public string name;
+
+    public Student(string name)
+    {
+        numberOfStudents++;
+        this.name = name;
+    }
+}
+
+class Pr1Students
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        while (input != "End")
+        {
+            string name = input;
+            Student st = new Student(name);
+            input = Console.ReadLine();
+        }
+        Console.WriteLine(Student.numberOfStudents);
+    }
+}
