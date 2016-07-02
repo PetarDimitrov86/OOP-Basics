276196f523fd54c2d05a04caa04dae630cebd821
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-02 18:44:15 +0300

    Add files via upload

---
+++ StaticMembers/04-Pr4BeerCounter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+public class BeerCounter
+{
+    public static int beerInStock;
+    public static int beersDrankCount;
+
+    public static void BuyBeer(int bottlesCount)
+    {
+        beerInStock += bottlesCount;
+    }
+
+    public static void DrinkBeer(int bottlesCount)
+    {
+        beersDrankCount += bottlesCount;
+        beerInStock -= bottlesCount;
+    }
+}
+
+class Pr4BeerCounter
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        while (input != "End")
+        {
+            int[] beerInfo = input.Split().Select(int.Parse).ToArray();
+            int beerBought = beerInfo[0];
+            int beerDrank = beerInfo[1];
+            BeerCounter.BuyBeer(beerBought);
+            BeerCounter.DrinkBeer(beerDrank);
+            input = Console.ReadLine();
+        }
+        Console.WriteLine($"{BeerCounter.beerInStock} {BeerCounter.beersDrankCount}");
+    }
+}
