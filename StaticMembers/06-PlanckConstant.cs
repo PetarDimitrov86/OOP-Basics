78350dc8ded55160ccee84f3de0b148819a5527c
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-02 19:22:56 +0300

    Add files via upload

---
+++ StaticMembers/06-PlanckConstant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Calculation
+{
+    public static double plankConst = 6.62606896e-34;
+    public static double PI = 3.14159;
+
+    public static double ReturnResult()
+    {
+        double result = plankConst/(2*PI);
+        return result;
+    }
+}
+
+class PlanckConstant
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Calculation.ReturnResult());
+    }
+}
