a8c36ad8097845096d6fd67f410c940c46690de6
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-02 18:24:47 +0300

    Add files via upload

---
+++ StaticMembers/02-UniqueStudentNames.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Student
+{
+    public static HashSet<Student> students;
+    public string name;
+
+    static Student()
+    {
+        students = new HashSet<Student>();
+    }
+
+    public Student(string name)
+    {
+        this.name = name;
+    }
+
+    public override bool Equals(object other)
+    {
+        return this.GetHashCode().Equals(other.GetHashCode());
+    }
+
+    public override int GetHashCode()
+    {
+        return this.name.GetHashCode();
+    }
+}
+
+class UniqueStudentNames
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        while (input != "End")
+        {
+            Student.students.Add(new Student(input));
+            input = Console.ReadLine();
+        }
+        Console.WriteLine(Student.students.Count);
+    }
+}
